91ddda13a889825f9125efe67e097c6a5a4dd503
FilixXxMal <maldifassi.filippo.maria@iisgalvanimi.edu.it>
2019-08-26 15:10:31 +0000

    commit 1

---
+++ prenotazione-maldifassi/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PrenotazioneViewComponent } from './prenotazione-view/prenotazione-view.component';
+import { PrenotazioneComponent } from './prenotazione/prenotazione.component';
+import { PrenotazioniComponent } from './prenotazioni/prenotazioni.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrenotazioneViewComponent,
+    PrenotazioneComponent,
+    PrenotazioniComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ prenotazione-maldifassi/src/app/prenotazione.ts
@@ -0,0 +1,8 @@
+export class Prenotazione{
+  nome:string
+  cognome:string
+  indirizzo:string
+  telefono:string
+  DataPrenotazione:string
+  OraPrenotazione:string
+}
